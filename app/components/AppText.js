cfe917e53dc7f791ee46d6dd7a645dfc2fa7aa88
sshingh67 <singhsharandeep676@gmail.com>
2020-06-28 22:17:09 -0700

    added styling to font

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, StyleSheet, Platform } from "react-native";
 
-import styles from "./styles";
+import colors from "../config/colors";
 
-function AppText({ children }) {
-  return <Text style={styles.text}>{children}</Text>;
+function AppText({ children, style }) {
+  return <Text style={[styles.text, style]}>{children}</Text>;
 }
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    fontFamily: Platform.OS === "andriod" ? "Roboto" : "Avenir",
+  },
+});
 
 export default AppText;
