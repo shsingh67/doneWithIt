b789d1ade0e1eea23f7dc0974f34d9535e50657a
sshingh67 <singhsharandeep676@gmail.com>
2020-06-29 17:26:30 -0700

    added style prop

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -2,13 +2,14 @@ import React from "react";
 import Constants from "expo-constants";
 import { StyleSheet, SafeAreaView } from "react-native";
 
-function Screen({ children }) {
-  return <SafeAreaView style={styles.screen}>{children}</SafeAreaView>;
+function Screen({ children, style }) {
+  return <SafeAreaView style={[styles.screen, style]}>{children}</SafeAreaView>;
 }
 
 const styles = StyleSheet.create({
   screen: {
     paddingTop: Constants.statusBarHeight,
+    flex: 1,
   },
 });
 
