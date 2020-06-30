8362784e5a20bf84b758a771896317b66517b861
sshingh67 <singhsharandeep676@gmail.com>
2020-06-29 17:27:31 -0700

    separated import statements

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -3,6 +3,7 @@ import { View, Image, StyleSheet } from "react-native";
 
 import AppText from "../components/AppText";
 import ListItem from "../components/ListItem";
+
 import colors from "../config/colors";
 
 function ListingDetailsScreen(props) {
