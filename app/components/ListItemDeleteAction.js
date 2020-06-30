99f18bf87cb721ef628d1918142ed34bad3d4364
sshingh67 <singhsharandeep676@gmail.com>
2020-06-29 17:26:03 -0700

    separated import statements

--- app/components/ListItemDeleteAction.js
+++ app/components/ListItemDeleteAction.js
@@ -3,6 +3,7 @@ import { View, StyleSheet } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import colors from "../config/colors";
+
 import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 
 function ListItemDeleteAction({ onPress }) {
